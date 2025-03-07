6142205c06b5a1f0749985d381180d4bd490f276
Ismail <ismail.jacoby@gmail.com>
2025-03-07 11:49:53 +0100

    Backend: Backend Init & application.yml

---
+++ connect-backend/src/test/java/com/ismailjacoby/connectbackend/ConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismailjacoby.connectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
